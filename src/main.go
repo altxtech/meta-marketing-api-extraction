4b6e087bdbc21e5598c35f93225dd3075c2fc1d6
André Luiz Tiago Soares <asoares@covidclinic.org>
2023-08-21 16:14:10 -0300

    Finished pagination

--- src/main.go
+++ src/main.go
@@ -10,93 +10,106 @@ import (
 	"github.com/Valgard/godotenv"
 )
 
-type PagingInfo struct {
-	Cursors struct {
-		Before string `json:"before"`
-		After string `json:"after"`
+func exec_request(req http.Request) (map[string]interface{}, error) {
+	
+	// Create objet to write results into
+	var data map[string]interface{}
+
+	resp, err := http.DefaultClient.Do(&req)
+	if err != nil {
+		return data, err
 	}
-	Next string `json:"next"`
+	defer resp.Body.Close()
+
+	// Evaluate status code
+	if resp.StatusCode != 200 {
+		return data, fmt.Errorf("Status code %d", resp.StatusCode)
+	}
+
+	// Parse the json data and return
+	content, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return data, err
+	}
+	json.Unmarshal(content, &data)
+	return data, nil
 }
 
-type Campaign struct {
-	ID string `json:"id"`
-}
-type CampaignEdge []Campaign
+func save_data(data map[string]interface{}, name string) error {
 
-type GetCampaignsResponse struct {
-	Data CampaignEdge
-	Paging PagingInfo
+	// Encode object into json
+	json_data, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	// Write it file
+	err = os.WriteFile(name, json_data, 0666)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 // Global variables
 func main() {
-
-	// Load environgment
+	// Load environment
 	godotenv.Load("../.env")
 	account_id := os.Getenv("ACCOUNT_ID")
 	fmt.Println("Account id: ", account_id)
 	access_token := os.Getenv("ACCESS_TOKEN")
 	fmt.Println("Access token: ", access_token)
 
-	// Extract all campaigns
+	// Build the requests
 	base_url := "https://graph.facebook.com/v17.0/" + account_id
 	campaigns_path := "/campaigns"
 
-	// Build the request
 	req, err := http.NewRequest("GET", base_url + campaigns_path, nil)
 	if err != nil {
 		// TODO: Handle
 	}
 	req.URL.RawQuery = url.Values{
 		"access_token": { access_token },
+		"limit": { "100" },
+		"date_preset": { "maximum" },
 	}.Encode()
 
 	// Execute it
 	page_counter := 1
-	fmt.Println("Extracting page: ", page_counter)
-	resp, err := http.DefaultClient.Do(req)
+	data, err := exec_request(*req)
 	if err != nil {
-		fmt.Println("There was an error making the request!")
-		fmt.Println(err)
+		//TODO: Handle
 	}
-	defer resp.Body.Close()
+	// Save the results
+	filename := fmt.Sprintf("page_%d.json", page_counter)
+	err = save_data(data, filename)
+	if err != nil {
+		//TODO: handle
+	}
+	after_cursor := data["paging"].(map[string]interface{})["cursors"].(map[string]interface{})["after"]
 
-	var result GetCampaignsResponse
-	fmt.Println(resp.StatusCode)
-	content, _ := io.ReadAll(resp.Body)
-	fmt.Println(string(content))
-	json.NewDecoder(resp.Body).Decode(&result)
-	// print the next token for debugging purposes
-	fmt.Println(result.Paging.Next)
-	page_counter += 1
-	
-	// Save results so I can look at it
-	file, _ := json.MarshalIndent(result, "", "    ")
-	_ = os.WriteFile("first_page.json", file, 0644)
+	// Now paginate :D:
 
-	for result.Paging.Next != "" {
-		// Create request for next page
-		fmt.Println("Extracting page: ", page_counter)
-		req, err = http.NewRequest("GET", result.Paging.Next, nil) // The next URL already inclues all necessary parameters
-		if err != nil {
-			fmt.Println("Error building request")
-		}
-		// Execute request
-		resp, err = http.DefaultClient.Do(req)
+	for after_cursor != nil {
+		page_counter += 1
+		// Update the query paramenters
+		req.URL.RawQuery = url.Values{
+			"access_token": { access_token },
+			"limit": { "100" },
+			"date_preset": { "maximum" },
+			"after": { after_cursor.(string) },
+		}.Encode()
+
+		// Execute it
+		data, err = exec_request(*req)
 		if err != nil {
 			//TODO: Handle
 		}
-
-		// Execute it
-		resp, err := http.DefaultClient.Do(req)
+		// Save the results
+		filename = fmt.Sprintf("page_%d.json", page_counter)
+		err = save_data(data, filename)
 		if err != nil {
-			fmt.Println("There was an error making the request!")
-			fmt.Println(err)
+			//TODO: handle
 		}
-		defer resp.Body.Close()
-
-		json.NewDecoder(resp.Body).Decode(&result)
-		fmt.Println(result.Paging.Next)
-		page_counter += 1
+		after_cursor = data["paging"].(map[string]interface{})["cursors"].(map[string]interface{})["after"]
 	}
 }

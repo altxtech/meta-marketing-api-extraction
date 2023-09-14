d462dfe7e10de758cbe933a08e53376b2ced01a5
altxtech <andre.soares@altxtech.net>
2023-09-14 16:37:26 -0300

    basic tf configuration file

---
+++ main.tf
@@ -0,0 +1,53 @@
+# Variables
+variable "gcp_project" {
+  description = "Project to deploy to"
+  type        = string
+}
+
+variable "gcp_region" {
+  description = "Default Region to deploy to"
+  type        = string
+}
+
+variable "state_bucket" {
+  description = "The name of the Google Cloud Storage bucket"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Name of the project. Used for namespacing resources"
+  type        = string
+}
+
+variable "environment" {
+  description = "Deployment environment. Used for namespacing an some configurations"
+  type        = string
+  default     = "dev"
+}
+
+provider "google" {
+  project = var.gcp_project
+  region  = var.gcp_region
+}
+
+terraform {
+  backend "gcs" {
+    bucket = var.state_bucket
+  }
+}
+
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id                  = "facebook_ads_test"
+  description                 = "This is a test description"
+  location                    = var.gcp_region
+  default_table_expiration_ms = 3600000
+
+  labels = {
+    env = "default"
+  }
+
+  access {
+    role          = "OWNER"
+    user_by_email = google_service_account.bqowner.email
+  }
+}
